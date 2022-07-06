55ff7b66d2f1a78f4a569a01b654d6495b512847
Elihu Romero <elihuromero@outlook.com>
2022-07-06 17:19:23 -0500

    correo y pantallas de fallo y logrado ok

--- enviarCorreo.php
+++ enviarCorreo.php
@@ -1,54 +1,46 @@
 <?php
-    //Import PHPMailer classes into the global namespace
-    //These must be at the top of your script, not inside a function
     use PHPMailer\PHPMailer\PHPMailer;
     use PHPMailer\PHPMailer\SMTP;
     use PHPMailer\PHPMailer\Exception;
-
-    //Load Composer's autoloader
-    //require 'vendor/autoload.php';
 
     require 'PHPMailer/src/Exception.php';
     require 'PHPMailer/src/PHPMailer.php';
     require 'PHPMailer/src/SMTP.php';
 
-    function enviarCorreo()
+    function enviarCorreo($datos)
     {
-        //Create an instance; passing `true` enables exceptions
         $mail = new PHPMailer(true);
 
         try {
-            //Server settings
-            /*
-            $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-            $mail->isSMTP();                                            //Send using SMTP
-            $mail->Host       = 'smtp.office365.com';                     //Set the SMTP server to send through
-            $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-            $mail->Username   = 'elihuromero@outlook.com';                     //SMTP username
-            $mail->Password   = 'SinPass09+';                               //SMTP password
-            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-            $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-            */
-
-            //$mail = new PHPMailer();
             $mail->isSMTP();
             $mail->Host = 'smtp.mailtrap.io';
             $mail->SMTPAuth = true;
             $mail->Port = 2525;
             $mail->Username = 'd28af12396980f';
             $mail->Password = 'f1690b24f97113';
-            //Recipients
-            $mail->setFrom('elihuromero2@example.com', 'Mailer');
-            $mail->addAddress('elihuromero@example.com', 'Elihu Romero');     //Add a recipient
+            
+            $mail->setFrom('elihuromero2@outlook.com', 'Mailer');
+            $mail->addAddress('elihuromero@outlook.com', 'Elihu Romero');     //Add a recipient
 
-            //Content
-            $mail->isHTML(true);                                  //Set email format to HTML
-            $mail->Subject = 'Here is the subject';
-            $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+            $mail->isHTML(true);
+            $mail->Subject = 'Envio de datos';
+            //$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+            $message = '
+                <html>
+                <head>
+                <title>Datos enviados en formulario</title>
+                </head>
+                <body>
+                <p>Aqui estan tus datos enviados en el formulario:</p>
+                <div>'.$datos.'</div>
+                </body>
+                </html>
+                ';
+            $mail->Body = $message;
             $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
             $mail->send();
-            return 'Message has been sent';
+            return true;
         } catch (Exception $e) {
             return "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
         }
